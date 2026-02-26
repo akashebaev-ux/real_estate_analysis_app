393a99484b6308af1b599d26a2514389b8f71689
akashebaev-ux <akashebaev@gmail.com>
2026-02-26 19:19:05 +0100

    add a selenium scraper and the website url

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
+"""
+Real Estate Analysis App
+
+This script:
+- Connects to Google Sheets using gspread
+- Creates or opens a worksheet named with today's date
+- Uses Selenium WebDriver to open a real estate website
+- Prepares data for storage in Google Sheets
+
+Selenium is used to automate the browser and collect
+real estate data such as prices, locations, and links.
+"""
+
 import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -25,3 +42,13 @@ except:
         rows="10000",
         cols="20"
     )
+
+driver = webdriver.Chrome()
+
+driver.get("https://krisha.kz/prodazha/kvartiry/almaty/")
+
+# The website krisha.kz is a popular real estate listing site in Kazakhstan, 
+# where users can find apartments for sale in Almaty. The script opens this
+# website using Selenium WebDriver to prepare for data extraction. This website doesn't
+# have blockers and is accessible for scraping, making it a suitable choice for 
+# collecting real estate data.
